773658f11998201d98426362bba2e9f0a7e2a765
Andrew Davis <andrew4d3@gmail.com>
2019-04-16 22:45:34 -0400

    Create lesson01.js

---
+++ lessons/lesson01.js
@@ -0,0 +1,20 @@
+/*
+So far we have been able to map different values using observables (string, booleans, etc)
+But what if we wanted to map another observable?
+*/
+
+/* Importing dependencies */
+import { interval, fromEvent } from 'rxjs' // Observable creators
+import { map } from 'rxjs/operators' // Operators
+
+// Creating an observable from an event. It will emit every time the dom gets clicked
+const clickObservable = fromEvent(document, 'click')
+
+// Mapping the observable to return a new observable (interval) every time the dom gets clicked
+const clockObservable = clickObservable
+  .pipe(map(click => interval(1000)))
+
+// Subscribing to the the past observable, now every time the dom gets clicked a new sequence of number will start printing on console
+clockObservable
+  .subscribe(clock => clock.subscribe(tick => console.log(tick))) // tick is gomming from internal
+
